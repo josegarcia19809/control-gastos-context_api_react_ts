7ed260cd57405b3280cf389f9b0927756c5631ad
josegarcia19809 <josegarcia19809@gmail.com>
2024-08-10 07:00:30 -0600

    PROYECTO- Control de Gastos Context API: Creando el Formulario para Definir el presupuesto

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,8 +1,38 @@
+import {ChangeEvent, useState} from "react";
+
 function BudgetForm() {
+
+    const [budget, setBudget] = useState(0);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setBudget(e.target.valueAsNumber)
+    }
+
     return (
-        <>
-            <p>Formulario</p>
-        </>
+        <form className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget"
+                       className="text-4xl text-blue-600 font-bold text-center">
+                    Definir presupuesto
+                </label>
+                <input
+                    id="budget"
+                    name="budget"
+                    type="number"
+                    className="w-full bg-white border border-gray-200 p-2"
+                    placeholder="Define tu presupuesto"
+                    value={budget}
+                    onChange={handleChange}
+                />
+                <input
+                    type="submit"
+                    value="Definir presupuesto"
+                    className="w-full bg-blue-600 hover:bg-blue-700 cursor-pointer p-2
+                    text-white font-black uppercase"
+
+                />
+            </div>
+        </form>
     );
 }
 
