50e875df5d9a5a856a6e3b0572902b67522ecc41
josegarcia19809 <josegarcia19809@gmail.com>
2024-08-11 15:00:48 -0600

    PROYECTO- Control de Gastos Context API: Agregando el Context a nuestra aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import BudgetForm from "./components/BudgetForm.tsx";
+import {useContext} from "react";
+import {BudgetContext} from "./context/BudgetContext.tsx";
 
 function App() {
+
+    const context= useContext(BudgetContext);
+    console.log(context);
+
     return (
         <>
             <header className="bg-blue-600 py-8 max-h-72">
