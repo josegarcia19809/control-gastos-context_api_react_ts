ef6dd615fb59937758898a81defd05f119a2bfe7
Jose Garcia <josegarcia19809@gmail.com>
2025-02-21 07:57:14 -0600

    PROYECTO- Control de Gastos Context API: Mostrando los gastos

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,4 +1,6 @@
 import {Expense} from "../types";
+import {formatDate} from "../helpers";
+import AmountDisplay from "./AmountDisplay.tsx";
 
 type ExpenseDetailProps = {
     expense: Expense
@@ -6,7 +8,18 @@ type ExpenseDetailProps = {
 
 function ExpenseDetail({expense}: ExpenseDetailProps) {
     return (
-        <></>
+        <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+            <div>
+
+            </div>
+
+            <div>
+                <p>{expense.expenseName}</p>
+                <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+            </div>
+
+            <AmountDisplay amount={expense.amount}/>
+        </div>
     );
 }
 
