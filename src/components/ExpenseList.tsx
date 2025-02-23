e533ffbb4d585b9b2e01827440815bfdfac783b2
Jose Garcia <josegarcia19809@gmail.com>
2025-02-23 13:18:40 -0600

    PROYECTO- Control de Gastos Context API: Mostrar los gastos de la categoría seleccionada

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -5,17 +5,21 @@ import ExpenseDetail from "./ExpenseDetail.tsx";
 function ExpenseList() {
 
     const {state} = useBudget();
-    const isEmpty = useMemo(() => state.expenses.length <= 0, [state.expenses]);
+    const filteredExpenses = state.currentCategory ?
+        state.expenses.filter(expense => expense.category === state.currentCategory) :
+        state.expenses;
+
+    const isEmpty = useMemo(() => filteredExpenses.length <= 0, [filteredExpenses]);
 
     return (
         <div className="mt-10">
             {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No hay gastos</p> : (
                 <>
                     <p className="text-gray-600 text-2xl font-bold my-5">Listado de gastos</p>
-                    {state.expenses.map((expense) => (
+                    {filteredExpenses.map((expense) => (
                         <ExpenseDetail
-                        key={expense.id}
-                        expense={expense}
+                            key={expense.id}
+                            expense={expense}
                         />
                     ))}
                 </>
