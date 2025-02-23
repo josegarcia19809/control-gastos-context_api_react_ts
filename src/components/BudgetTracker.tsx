eb2582f25c9a953eb2000990f735a69b4cf0b6be
Jose Garcia <josegarcia19809@gmail.com>
2025-02-23 08:04:43 -0600

    PROYECTO- Control de Gastos Context API: Agregando una gráfica circular interactiva

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,14 +1,27 @@
 import AmountDisplay from "./AmountDisplay.tsx";
 import {useBudget} from "../hooks/useBudget.ts";
+import {CircularProgressbar, buildStyles} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function BudgetTracker() {
 
     const {state, totalExpenses, remainingBudget} = useBudget();
 
+    const percentage = +((totalExpenses / state.budget) * 100).toFixed(2);
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             <div className="flex justify-center">
-                <img src="/grafico.jpg" alt="Grafico de gastos"/>
+                <CircularProgressbar
+                    value={percentage}
+                    styles={buildStyles({
+                        pathColor: percentage===100 ? "#DC2626": "#3b82f6",
+                        trailColor: "#F5F5F5",
+                        textSize: 8,
+                        textColor: percentage===100 ? "#DC2626": "#3b82f6"
+                    })}
+                    text={`${percentage}% Gastado`}
+                />
             </div>
             <div className="flex flex-col justify-center items-center gap-8">
                 <button
